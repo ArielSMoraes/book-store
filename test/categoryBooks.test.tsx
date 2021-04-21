1ce30fe752c90b96c36a9a341a0b3297b09c4cd8
Ariel Santos Moraes <ariel.s.moraes@gmail.com>
2021-04-21 20:53:51 -0300

    tests for categoryBooks

--- test/categoryBooks.test.tsx
+++ test/categoryBooks.test.tsx
@@ -3,18 +3,29 @@ import Adapter from 'enzyme-adapter-react-16'
 import React from 'react'
 
 Enzyme.configure({ adapter: new Adapter() })
-// import toJson from 'enzyme-to-json'
+import toJson from 'enzyme-to-json'
 
 import CategoryBooks, { H2 } from '../src/categoryBooks'
+import ListBooks from '../src/listBooks'
 
 describe('CategoryBooks', () => {
   test('category has a title ', () => {
     const wrapper = shallow(<CategoryBooks category='Action' />)
+
     expect(wrapper.find(H2)).toHaveLength(1)
   })
 
-  test('category name is used as title ', () => {
+  test('category name is used as title', () => {
     const wrapper = shallow(<CategoryBooks category='Action' />)
+
     expect(wrapper.find(H2).text()).toBe('Action')
+  })
+
+  test('should call ListBooks with correct term', () => {
+    const wrapper = shallow(<CategoryBooks category='Action' />)
+
+    const listBooksTerm = toJson(wrapper.find(ListBooks)).props.term
+
+    expect(listBooksTerm).toBe('Action')
   })
 })
