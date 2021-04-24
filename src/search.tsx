b6decf259fc0a315485accfff5b77ac2718cc9b3
Ariel Santos Moraes <ariel.s.moraes@gmail.com>
2021-04-24 16:23:35 -0300

    using state on search component

--- src/search.tsx
+++ src/search.tsx
@@ -1,14 +1,32 @@
-import React, { FunctionComponent } from 'react'
-import { FaSearch } from 'react-icons/fa'
+import React, { FunctionComponent, useState } from 'react'
+import { FaSearch, FaBacon } from 'react-icons/fa'
 import { Img, SearchWrapper } from './styled-components/styledSearch'
 
 const Search: FunctionComponent = () => {
-  return (
-    <SearchWrapper>
-      <Img src='assets/logo.jpg'/>
-      <FaSearch size='22pt' color='#2D2D2E' />
-    </SearchWrapper>
-  )
+  const [searchActive, setSearchActive] = useState(false)
+
+  const toggleSearchActive = () => {
+    setSearchActive(!searchActive)
+  }
+
+  const InativeSearch = () => {
+    return (
+      <SearchWrapper>
+        <Img src='assets/logo.jpg'/>
+        <FaSearch size='22pt' color='#2D2D2E' onClick={toggleSearchActive}/>
+      </SearchWrapper>
+    )
+  }
+
+  const ActiveSearch = () => {
+    return (
+      <SearchWrapper>
+        <FaBacon size='22pt' color='#2D2D2E' onClick={toggleSearchActive}/>
+      </SearchWrapper>
+    )
+  }
+
+  return searchActive ? <ActiveSearch /> : <InativeSearch />
 }
 
 export default Search
